502a7a216c894abebf34f7da45773774ba040390
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-05-03 06:44:49 +0000

    [add]フォロー機能の非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,12 @@
 class RelationshipsController < ApplicationController
 
   def create
-    @user = current_user.follow(params[:user_id])
+    current_user.follow(params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   def destroy
-    @user = current_user.unfollow(params[:user_id])
+    current_user.unfollow(params[:user_id])
+    @user = User.find(params[:user_id])
   end
 end
