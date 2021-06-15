34321ec749cb8cd8793f25fa90df308ed392450f
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-06-15 07:39:19 +0000

    [add]記述変更

--- app/models/log.rb
+++ app/models/log.rb
@@ -88,7 +88,7 @@ class Log < ApplicationRecord
       save_notification_comment!(current_user, log_comment_id, temp_id['user_id'])
     end
     # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, log_comment_id, user_id) if temp_ids.blank?
+    save_notification_comment!(current_user, log_comment_id, user_id)
   end
 
   def save_notification_comment!(current_user, log_comment_id, visited_id)
