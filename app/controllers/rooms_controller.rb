c0454a321c76fcf315418d4ef95dbc792bc48331
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-08-07 11:25:23 +0000

    [update]冗長化の改善

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,8 +3,8 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.create
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    Entry.create(room_id: @room.id, user_id: current_user.id)
+    Entry.create(rooms_params.merge(room_id: @room.id))
     redirect_to room_path(@room.id)
   end
 
@@ -27,5 +27,11 @@ class RoomsController < ApplicationController
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+  
+  private
+  
+  def rooms_params
+    params.require(:entry).permit(:user_id, :room_id)
   end
 end
